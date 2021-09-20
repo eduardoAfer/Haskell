7c58184cc59c44de74e61af134135ff1f6782b34
Eduardo Ferreira <80621444+eduardoAfer@users.noreply.github.com>
2021-09-20 15:19:22 +0100

    code

---
+++ week4/.stack-work/dist/x86_64-osx/Cabal-3.2.1.0/build/autogen/Paths_semana04.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE NoRebindableSyntax #-}
+{-# OPTIONS_GHC -fno-warn-missing-import-lists #-}
+module Paths_semana04 (
+    version,
+    getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir,
+    getDataFileName, getSysconfDir
+  ) where
+
+import qualified Control.Exception as Exception
+import Data.Version (Version(..))
+import System.Environment (getEnv)
+import Prelude
+
+#if defined(VERSION_base)
+
+#if MIN_VERSION_base(4,0,0)
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#else
+catchIO :: IO a -> (Exception.Exception -> IO a) -> IO a
+#endif
+
+#else
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#endif
+catchIO = Exception.catch
+
+version :: Version
+version = Version [0,1,0,0] []
+bindir, libdir, dynlibdir, datadir, libexecdir, sysconfdir :: FilePath
+
+bindir     = "/Volumes/GoogleDrive-113371203763727647694/O meu disco/gitHub/Haskell/week4/.stack-work/install/x86_64-osx/e03d72b77e36b85421bb506cb5b5d30189907bc5854955490703c9679552fc5e/8.10.4/bin"
+libdir     = "/Volumes/GoogleDrive-113371203763727647694/O meu disco/gitHub/Haskell/week4/.stack-work/install/x86_64-osx/e03d72b77e36b85421bb506cb5b5d30189907bc5854955490703c9679552fc5e/8.10.4/lib/x86_64-osx-ghc-8.10.4/semana04-0.1.0.0-B9Olf0S1DFVyMQ9pmxAj"
+dynlibdir  = "/Volumes/GoogleDrive-113371203763727647694/O meu disco/gitHub/Haskell/week4/.stack-work/install/x86_64-osx/e03d72b77e36b85421bb506cb5b5d30189907bc5854955490703c9679552fc5e/8.10.4/lib/x86_64-osx-ghc-8.10.4"
+datadir    = "/Volumes/GoogleDrive-113371203763727647694/O meu disco/gitHub/Haskell/week4/.stack-work/install/x86_64-osx/e03d72b77e36b85421bb506cb5b5d30189907bc5854955490703c9679552fc5e/8.10.4/share/x86_64-osx-ghc-8.10.4/semana04-0.1.0.0"
+libexecdir = "/Volumes/GoogleDrive-113371203763727647694/O meu disco/gitHub/Haskell/week4/.stack-work/install/x86_64-osx/e03d72b77e36b85421bb506cb5b5d30189907bc5854955490703c9679552fc5e/8.10.4/libexec/x86_64-osx-ghc-8.10.4/semana04-0.1.0.0"
+sysconfdir = "/Volumes/GoogleDrive-113371203763727647694/O meu disco/gitHub/Haskell/week4/.stack-work/install/x86_64-osx/e03d72b77e36b85421bb506cb5b5d30189907bc5854955490703c9679552fc5e/8.10.4/etc"
+
+getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
+getBinDir = catchIO (getEnv "semana04_bindir") (\_ -> return bindir)
+getLibDir = catchIO (getEnv "semana04_libdir") (\_ -> return libdir)
+getDynLibDir = catchIO (getEnv "semana04_dynlibdir") (\_ -> return dynlibdir)
+getDataDir = catchIO (getEnv "semana04_datadir") (\_ -> return datadir)
+getLibexecDir = catchIO (getEnv "semana04_libexecdir") (\_ -> return libexecdir)
+getSysconfDir = catchIO (getEnv "semana04_sysconfdir") (\_ -> return sysconfdir)
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  dir <- getDataDir
+  return (dir ++ "/" ++ name)
--- week4/src/Exercicios.hs
+++ week4/src/Exercicios.hs
@@ -3,47 +3,88 @@
 
 module Exercicios where
 
-ehTriangulo :: TipoTriangulo -> Bool
-ehTriangulo (TipoTriangulo a b c) | abs (b - c) < a && a < b + c ||
-                                    abs (a - c) < b && b < a + c ||
-                                    abs (a - b) < c && c < a + b = True
-                                   |otherwise = False
-data TipoTriangulo = TipoTriangulo Int Int Int
-  deriving (Show)
+ ehTriangulo :: (Num a, Ord a) => a -> a -> a -> Bool
+ ehTriangulo  a b c = abs (b - c) < a && a < b + c ||
+                      abs (a - c) < b && b < a + c ||
+                      abs (a - b) < c && c < a + b 
+                    
 
-determinaTriangulo :: TipoTriangulo -> String
-determinaTriangulo (TipoTriangulo a b c) | not $ ehTriangulo (TipoTriangulo a b c) = "Não é possivel construir um Triângulo"
-                                         | a == b && a == c = "Equilátero"
-                                         | a == b && a /= c ||
-                                           b == c && c /= a ||
-                                           c == a && b /= c = "Isósceles"
-                                         | otherwise = "Escaleno" 
-                                        
+data TipoTriangulo = Escaleno | Isosceles | Equilatero | Nenhum
+   deriving (Show)
+
+ determinaTriangulo :: (Num a, Ord a) -> a -> a -> a -> TipoTriangulo 
+ determinaTriangulo  a b c 
+                     | a == b && a == c = Equilátero
+                     | a == b && a /= c ||
+                       b == c && c /= a ||
+                       c == a && b /= c = Isósceles
+                     | otherwise = Escaleno 
+                                    
 
 data Horario = HoraMinuto Int Int
   deriving (Show, Read)
 
 somarHorario :: Horario -> Horario -> Horario 
-somarHorario (HoraMinuto h m)  = HoraMinuto (h + (m mod 60)) ()
+somarHorario (HoraMinuto h m) (HoraMinuto h2 m2) | m + m2 >= 60  = HoraMinuto (h + h2 + a + b) (mod m 60)
+                                                 | otherwise = HoraMinuto (h + h2) (m + m2)
+  where minToHora_1 = div m 60
+        minToHora_2 = div m2 60                                         
 
-data Periodo =
-  DummyPeriodo
-  deriving (Show)
 
-horarioToPeriodo = undefined
 
-data Medida =
-  DummyMedida
-  deriving (Show, Read)
 
-converter = undefined
+ data Periodo = Madrugada | Manha | Tarde | Noite
+   deriving (Show)
 
-data Nota =
-  DummyNota
-  deriving (Show)
+ horarioToPeriodo :: Horario -> Periodo
+ horarioToPeriodo h m | h >= 0 && h <= 5 && m >= 0 && m <= 59 = Madrugada
+                      | h >= 6 && h <= 11 && m >= 0 && m <= 59 = Manha
+                      | h >= 12 && h <= 17 && m >= 0 && m <= 59 = Tarde
+                      | otherwise = Noite
 
-data ModoGrego =
-  DummyModoGrego
-  deriving (Show, Read)
+class Booleable a where
+  toBool :: a -> Bool
 
-gerarModo = undefined
+instance Booleanable Bool where
+  toBool  =  id --  equal to toBool b =  b
+
+
+instance Booleanable Int where
+  toBool i = i /= 0 -- equal to toBool = (/=) 0
+
+
+instance Booleanable Float where
+  toBool f = f /= 0 &&  not (isNegativeZero f)
+
+
+instance Booleanable String where
+  toBool s = not (null s) && s /= "0"
+
+instance Booleanable [Int] where
+  toBool = not . null
+
+
+data Medida = Polegada | Palmo | Pe | Jarda | Milha
+   deriving (Show, Read, Ord) -- o Ord vai automaticamente efetuar a representação de maior para menor nos construtores
+
+fatorConversao :: Fractional a => Medida -> Medida -> a
+fatorConversao de       para     | de    para = recip $ fatorConversao para de
+fatorConversao Polegada Polegada = 1
+fatorConversao Palmo    Polegada = 9
+fatorConversao Pe       Polegada = 12
+fatorConversao Jarda    Polegada = 36
+fatorConversao Milha    Polegada = 63360
+
+
+ converter ::Fractional a => Medida -> Medida -> a -> a
+ converter de para x = x * fatorConversao de Polegada * fatorConversao Polegada para
+
+data Nota = Do | Re | Mi | Fa | Sol | La | Si 
+  deriving (Show, Enum)
+
+data ModoGrego = Jonio | Dorico | Frigio | Lidio | Mixolidio | Eolio | Locrio
+  deriving (Show, Read, Enum)
+
+gerarModo :: Int -> ModoGrego -> [Nota]
+gerarModo n modo = drop (fromEnum modo) $ repeat (Do.. Si)
+
